6e75811b00da565919b1a9b299228d1fff7d5100
Ubaid ur Rehman <ubaidshan007@gmail.ocm>
2018-12-15 20:40:54 +0500

    adding authentication

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let     express                     =   require("express"),
-        app                         =   express();
+        app                         =   express(),
+        bodyParser                  =   require("body-parser"),
+        mongoose                    =   require("mongoose"),
+        admin                       =   require("./models/admin"),
+        passport                    =   require("passport");
+
 
 
 
@@ -7,15 +12,58 @@ let     express                     =   require("express"),
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(admin.createStrategy());
+passport.serializeUser(admin.serializeUser());
+passport.deserializeUser(admin.deserializeUser());
 
 
 
-app.use("/", function(req, res)
-{
-    res.render("index/Homepage");
+//connection
+let db_url = process.env.DATABASEURL || "mongodb://localhost/web_eng"
+
+mongoose.connect(db_url);
+
+var name = "ubaid";
+var password = "pakistan1234";
+var newUser = new admin({
+    username: name
+});
+admin.register(newUser, password, function (err, account) {
+    if (err) {
+        console.log(err);
+    }
 });
 
 
+
+
+
+
+app.get("/admin", function(req, res)
+{
+    res.render("admin/login");
+});
+
+// create login
+app.post("/admin", passport.authenticate("local", {
+    failureRedirect : "/admin",
+}), function(req, res)
+{
+    res.send("Authentication Successfull");
+});
+
+
+
+
+app.get("/", function(req, res)
+{
+    res.render("index/Homepage");
+});
 
 
 
