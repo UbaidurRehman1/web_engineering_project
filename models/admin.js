119d8d8f0c0d0f82796e25a217645ebf29decc0f
Ubaid ur Rehman <ubaidshan007@gmail.ocm>
2018-12-15 20:44:00 +0500

    added

---
+++ models/admin.js
@@ -0,0 +1,15 @@
+const   mongoose                =   require("mongoose"),
+        passportLocalMongoose   =   require("passport-local-mongoose");
+
+
+let userSchema = new mongoose.Schema(
+    {
+        adminName: String,
+        password: String
+    }
+);
+
+userSchema.plugin(passportLocalMongoose);
+
+
+module.exports = mongoose.model("admin", userSchema);
