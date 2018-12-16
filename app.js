1ebad7e74370c59b79602f89c1e602d11062a184
Ameer Hamza <ahamza.bese16seecs@seecs.edu.pk>
2018-12-16 16:17:14 +0500

    user data updated

--- app.js
+++ app.js
@@ -28,16 +28,16 @@ let db_url = process.env.DATABASEURL || "mongodb://localhost/web_eng"
 
 mongoose.connect(db_url);
 
-var name = "ubaid";
-var password = "pakistan1234";
-var newUser = new admin({
-    username: name
-});
-admin.register(newUser, password, function (err, account) {
-    if (err) {
-        console.log(err);
-    }
-});
+// var name = "ubaid";
+// var password = "pakistan1234";
+// var newUser = new admin({
+//     username: name
+// });
+// admin.register(newUser, password, function (err, account) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
 
 
 
