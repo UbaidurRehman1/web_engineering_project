dd7be920660cfa25a34572ccc2d157152e3db400
Ubaid ur Rehman <ubaidshan007@gmail.ocm>
2018-12-16 18:06:49 +0500

    adding js file

---
+++ assests/js/application.js
@@ -0,0 +1,14 @@
+$(function()
+{
+    update();
+});
+
+function update()
+{
+    console.log("Chal ja");
+    $(".jobUpdate").click(function(date)
+    {
+        var $this = this;
+        console.log($this);
+    });
+}
